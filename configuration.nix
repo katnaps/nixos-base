3785fa52e0a46b321609b2494835131e120e743b
katnaps <59246759+katnaps@users.noreply.github.com>
2026-06-29 20:33:58 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -61,7 +61,7 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.myUsername = {
+  users.users.katnaps = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
