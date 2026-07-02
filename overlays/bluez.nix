4eb0884d2331f81ca9c21c1dc2b238238df20f68
katnaps <59246759+katnaps@users.noreply.github.com>
2026-07-03 03:01:43 +0800

    updated bluez.nix sources change with github fallback

--- overlays/bluez.nix
+++ overlays/bluez.nix
@@ -1,14 +1,28 @@
 final: prev: {
-  bluez = prev.bluez.overrideAttrs (old: {
+  bluez = prev.bluez.overrideAttrs (old: rec {
     version = "5.85";
-    src = prev.fetchFromGitHub {
-      owner = "bluez";
-      repo = "bluez";
-      rev = "5.85";
-      hash = "sha256-v9OpRed1eVxNdPeo4X2Jh1qKfyFcoWfzmguQbRcJd78=";
+
+    # If kernel url is down, comment this block and uncomment fetchFromGitHub block
+    src = prev.fetchurl {
+      urls = [
+        "mirror://kernel/linux/bluetooth/bluez-${version}.tar.xz"
+        "https://www.kernel.org/pub/linux/bluetooth/bluez-${version}.tar.xz"
+      ];
+      hash = "sha256-rQKOSSVLxFUaE/CP55BMY9ArplDXe+iuFbs7CgrZSm8=";
     };
 
-    # Remove patches that don't apply
+    # src = prev.fetchFromGitHub {
+    #   owner = "bluez";
+    #   repo = "bluez";
+    #   rev = "5.85";
+    #   hash = "sha256-MT0FMo/ENKq7XR3wBHKe6Xzx+riGUM6pNqiJrS6NH64=";
+    # };
+
+    # Use autoreconfHook only when using fetchFromGitHub
+    # nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.autoreconfHook ];
+
+    # Keeping it lightweight and fast by removing autoreconfHook
+    # and clearing older patches cleanly
     patches = [ ];
   });
 }
