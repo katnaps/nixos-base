a327e9dffd49d2b073490a33ef850cf17fccbc5f
katnaps <59246759+katnaps@users.noreply.github.com>
2026-07-02 22:29:46 +0800

    rename bluez-585.nix to bluez.nix and change source to github

---
+++ overlays/bluez.nix
@@ -0,0 +1,14 @@
+final: prev: {
+  bluez = prev.bluez.overrideAttrs (old: {
+    version = "5.85";
+    src = prev.fetchFromGitHub {
+      owner = "bluez";
+      repo = "bluez";
+      rev = "5.85";
+      hash = "sha256-v9OpRed1eVxNdPeo4X2Jh1qKfyFcoWfzmguQbRcJd78=";
+    };
+
+    # Remove patches that don't apply
+    patches = [ ];
+  });
+}
