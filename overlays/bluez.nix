c559cc5675c3b098ddff31472aaa5b3099467c8d
katnaps <59246759+katnaps@users.noreply.github.com>
2026-07-03 10:46:21 +0800

    formatted bluez.nix and change overlays format

--- overlays/bluez.nix
+++ overlays/bluez.nix
@@ -1,28 +1,30 @@
 final: prev: {
-  bluez = prev.bluez.overrideAttrs (old: rec {
-    version = "5.85";
+  bluez = prev.bluez.overrideAttrs (
+    finalAttrs: _prev: {
+      version = "5.85";
 
-    # If kernel url is down, comment this block and uncomment fetchFromGitHub block
-    src = prev.fetchurl {
-      urls = [
-        "mirror://kernel/linux/bluetooth/bluez-${version}.tar.xz"
-        "https://www.kernel.org/pub/linux/bluetooth/bluez-${version}.tar.xz"
-      ];
-      hash = "sha256-rQKOSSVLxFUaE/CP55BMY9ArplDXe+iuFbs7CgrZSm8=";
-    };
+      # If kernel url is down, comment this block and uncomment fetchFromGitHub block
+      src = final.fetchurl {
+        urls = [
+          "mirror://kernel/linux/bluetooth/bluez-${finalAttrs.version}.tar.xz"
+          "https://www.kernel.org/pub/linux/bluetooth/bluez-${finalAttrs.version}.tar.xz"
+        ];
+        hash = "sha256-rQKOSSVLxFUaE/CP55BMY9ArplDXe+iuFbs7CgrZSm8=";
+      };
 
-    # src = prev.fetchFromGitHub {
-    #   owner = "bluez";
-    #   repo = "bluez";
-    #   rev = "5.85";
-    #   hash = "sha256-MT0FMo/ENKq7XR3wBHKe6Xzx+riGUM6pNqiJrS6NH64=";
-    # };
+      # src = final.fetchFromGitHub {
+      #   owner = "bluez";
+      #   repo = "bluez";
+      #   rev = "5.85";
+      #   hash = "sha256-MT0FMo/ENKq7XR3wBHKe6Xzx+riGUM6pNqiJrS6NH64=";
+      # };
 
-    # Use autoreconfHook only when using fetchFromGitHub
-    # nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.autoreconfHook ];
+      # Use autoreconfHook only when using fetchFromGitHub
+      # nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.autoreconfHook ];
 
-    # Keeping it lightweight and fast by removing autoreconfHook
-    # and clearing older patches cleanly
-    patches = [ ];
-  });
+      # Keeping it lightweight and fast by removing autoreconfHook
+      # and clearing older patches cleanly
+      patches = [ ];
+    }
+  );
 }
